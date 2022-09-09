1345577e6bd5276593aa5385c4fbe5d4596dcace
Fardin <fakurian.fardin@gmail.com>
2022-09-09 19:48:11 +0430

    Add todo-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoPageComponent } from './todo-page/todo-page.component';
 import { TodoItemsComponent } from './todo-page/todo-items/todo-items.component';
+import { TodoListComponent } from './todo-page/todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoPageComponent, TodoItemsComponent],
+  declarations: [
+    AppComponent,
+    TodoPageComponent,
+    TodoItemsComponent,
+    TodoListComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
