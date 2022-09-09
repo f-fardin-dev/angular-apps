d0edf4825fe36fe81e56a55e7d7a83f42b9f918e
Fardin <fakurian.fardin@gmail.com>
2022-09-09 19:45:45 +0430

    Add todo-page and todo-items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodoPageComponent } from './todo-page/todo-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'todo-page', component: TodoPageComponent },
+  { path: '', redirectTo: 'todo-page', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
+import { TodoItemsComponent } from './todo-page/todo-items/todo-items.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoPageComponent, TodoItemsComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
