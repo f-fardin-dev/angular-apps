c05b6481f76c60b7cae9f41c6b25c2e93203d1fe
Fardin <fakurian.fardin@gmail.com>
2022-09-10 19:14:20 +0430

    Save and read todos into/from local storage

--- src/app/todo-page/todo-page.component.ts
+++ src/app/todo-page/todo-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { localStorageHelper } from 'src/utils/localStorageHelper';
 import { TODO } from './models/todos';
 
 @Component({
@@ -14,10 +15,16 @@ export class TodoPageComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const localStorageTodos = localStorageHelper.getItem<TODO[]>('ngTodos');
+    if (localStorageTodos) {
+      this.todos = localStorageTodos;
+    }
+  }
 
   addNewTodo(): void {
     this.todos.push({ title: this.title, description: this.description });
+    localStorageHelper.storeItem('ngTodos', this.todos);
     this.title = '';
     this.description = '';
   }
