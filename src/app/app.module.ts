bf39511071549549d8a5c9176b078f06bac0029d
Fardin <fakurian.fardin@gmail.com>
2022-09-12 22:14:38 +0430

    Add course card list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoPageComponent } from './todo-page/todo-page.component';
 import { TodoItemsComponent } from './todo-page/todo-items/todo-items.component';
 import { TodoListComponent } from './todo-page/todo-list/todo-list.component';
-import { FormsModule } from '@angular/forms';
 import { CoursePageComponent } from './course-page/course-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseCardListComponent } from './course-page/course-card-list/course-card-list.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +24,7 @@ import { MatIconModule } from '@angular/material/icon';
     TodoItemsComponent,
     TodoListComponent,
     CoursePageComponent,
+    CourseCardListComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +35,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatToolbarModule,
     MatListModule,
     MatIconModule,
+    MatTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/course-page/course-card-list/course-card-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-card-list',
+  templateUrl: './course-card-list.component.html',
+  styleUrls: ['./course-card-list.component.css']
+})
+export class CourseCardListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
