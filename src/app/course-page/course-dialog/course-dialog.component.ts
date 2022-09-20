603e2f20609e973efa6815c237b542ca6d0188f1
Fardin <fakurian.fardin@gmail.com>
2022-09-20 22:35:38 +0430

    Add course-dialog component

---
+++ src/app/course-page/course-dialog/course-dialog.component.ts
@@ -0,0 +1,46 @@
+import * as moment from 'moment';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { LoadingService } from 'src/app/loading/loading.service';
+import { MessagesService } from 'src/app/messages/messages.service';
+import { Course } from 'src/app/models/course';
+import { CoursesStore } from 'src/app/services/course.store';
+
+@Component({
+  selector: 'app-course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css'],
+  providers: [LoadingService, MessagesService],
+})
+export class CourseDialogComponent implements OnInit {
+  form: FormGroup;
+  course: Course;
+
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course,
+    private coursesStore: CoursesStore
+  ) {
+    this.course = course;
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  save() {
+    const changes = this.form.value;
+    this.coursesStore.saveCourse(this.course.id, changes).subscribe();
+    this.dialogRef.close(changes);
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+}
