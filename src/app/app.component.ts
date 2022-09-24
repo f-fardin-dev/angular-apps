d2ed0d8fb44ca81a83c3ab51d35ff6259d29acbb
Fardin <fakurian.fardin@gmail.com>
2022-09-24 19:45:39 +0330

    Update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthStore } from './services/auth.store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-apps';
+  constructor(public auth: AuthStore) {}
+  logout() {
+    this.auth.logout();
+  }
 }
