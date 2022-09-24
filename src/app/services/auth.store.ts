90079f56ab424a5834979002a3e13240b9600867
Fardin <fakurian.fardin@gmail.com>
2022-09-24 21:20:01 +0330

    Save user in localStorage

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable, shareReplay, tap } from 'rxjs';
 import { User } from '../models/user';
 
+const AUTH_DATA = 'auth_data';
 @Injectable({ providedIn: 'root' })
 export class AuthStore {
   private subject = new BehaviorSubject<User | null>(null);
@@ -12,6 +13,10 @@ export class AuthStore {
 
   constructor(private http: HttpClient) {
     this.isLoggedIn$ = this.user$.pipe(map((user) => !!user));
+    const user = localStorage.getItem(AUTH_DATA);
+    if (user) {
+      this.subject.next(JSON.parse(user));
+    }
   }
   login({
     email,
@@ -21,11 +26,15 @@ export class AuthStore {
     password: string;
   }): Observable<User> {
     return this.http.post<User>('/api/login', { email, password }).pipe(
-      tap((user) => this.subject.next(user)),
+      tap((user) => {
+        this.subject.next(user);
+        localStorage.setItem(AUTH_DATA, JSON.stringify(user));
+      }),
       shareReplay()
     );
   }
   logout() {
     this.subject.next(null);
+    localStorage.removeItem(AUTH_DATA);
   }
 }
