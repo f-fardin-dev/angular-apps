5d1b610300281001e54f5f7503f146ebe1c5f2d1
Fardin <fakurian.fardin@gmail.com>
2022-09-25 15:04:56 +0330

    Update route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CourseDetailComponent } from './course-page/course-detail/course-detail.component';
 import { CoursePageComponent } from './course-page/course-page.component';
+import { SearchLessonsComponent } from './course-page/search-lessons/search-lessons.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { TodoPageComponent } from './todo-page/todo-page.component';
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: 'todo-page', component: TodoPageComponent },
   { path: 'course-page', component: CoursePageComponent },
   { path: 'courses/:courseId', component: CourseDetailComponent },
+  { path: 'search-lessons', component: SearchLessonsComponent },
   { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
 ];
